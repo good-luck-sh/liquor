76aa1cfe38a77fd40013493f88fee2990d3a05af
초급개발 승히 <lion314@naver.com>
2022-01-14 01:50:13 +0900

    spring boot

---
+++ src/main/java/com/liquors/LiquorApplication.java
@@ -0,0 +1,13 @@
+package com.liquors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquorApplication.class, args);
+	}
+
+}
